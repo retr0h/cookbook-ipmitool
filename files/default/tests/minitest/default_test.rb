ec95cf14158f49c63d74174c8fef4e57143c6f23
John Dewey <john@dewey.ws>
2012-08-19 15:03:10 -0700

    + qualified constants and added service tests

--- files/default/tests/minitest/default_test.rb
+++ files/default/tests/minitest/default_test.rb
@@ -1,11 +1,21 @@
 describe_recipe "cookbook-ipmitool" do
-  include MiniTest::Chef::Assertions
-  include MiniTest::Chef::Context
-  include MiniTest::Chef::Resources
+  include ::MiniTest::Chef::Assertions
+  include ::MiniTest::Chef::Context
+  include ::MiniTest::Chef::Resources
 
   describe "package" do
-    it "installs ipmitool" do
+    it "installs" do
       package("ipmitool").must_be_installed
+    end
+  end
+
+  describe "services" do
+    it "runs as a daemon" do
+      service("ipmievd").must_be_running
+    end
+
+    it "boots on startup" do
+      service("ipmievd").must_be_enabled
     end
   end
 end
