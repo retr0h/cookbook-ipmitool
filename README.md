789440efaa7c6baa6c68c55a2f38d8d36de18df4
John Dewey <john@dewey.ws>
2014-02-11 20:47:52 -0800

    cleanup to work on travis

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ Installs/Configures ipmitool
 Requirements
 ============
 
-* Chef 0.8+
+* Chef 11
 
 Attributes
 ==========
@@ -25,25 +25,21 @@ Usage
 ```
 
 default
-----
+-------
 
 Installs/Configures ipmitool
 
 Testing
-=====
+=======
 
-This cookbook is using [ChefSpec](https://github.com/acrmp/chefspec) for
-testing. Should run the following before commiting. It will run your tests,
-and check for lint errors.
-
-    % ./run_tests.bash
+    $ rake
 
 License and Author
 ==================
 
 Author:: John Dewey (<john@dewey.ws>)
 
-Copyright 2012-2013, John Dewey
+Copyright 2012-2014, John Dewey
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -1,19 +1,21 @@
-require "chefspec"
+# encoding: UTF-8
 
-describe "ipmitool::default" do
+require_relative 'spec_helper'
+
+describe 'ipmitool::default' do
   before do
-    @chef_run = ::ChefSpec::ChefRunner.new.converge "ipmitool::default"
+    @chef_run = ChefSpec::Runner.new.converge 'ipmitool::default'
   end
 
-  it "installs package" do
-    @chef_run.should upgrade_package "ipmitool"
+  it 'installs package' do
+    @chef_run.should upgrade_package 'ipmitool'
   end
 
-  it "starts service" do
-    @chef_run.should start_service "ipmievd"
+  it 'starts service' do
+    @chef_run.should start_service 'ipmievd'
   end
 
-  it "enables service" do
-    @chef_run.should set_service_to_start_on_boot "ipmievd"
+  it 'enables service' do
+    @chef_run.should set_service_to_start_on_boot 'ipmievd'
   end
 end
